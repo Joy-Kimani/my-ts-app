a0e4227e9a286095069b7a63d0a026a7d6e14dbb
Joy-Kimani <kizengajoy@gmail.com>
2025-10-02 09:29:49 +0300

    my first ts setup

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const greet = (name) => {
+    return `Hello, ${name}!`;
+};
+console.log(greet("Joy"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// const greet = (name: string): string => {
+//   return `Hello, ${name}!`;
+// };
+
+// console.log(greet("Joy"));
+
+//data structures
+let num1:number = 2
+let num2:number =3
+
+console.log("Add "+ (num1+num2))
+console.log("Sub "+(num1-num2))
+
+//arrays
